18826194c287ba1502d518221461992bca84f767
Zamiquie <aymericbaquet@gmail.com>
2019-03-02 17:44:44 +0100

    -Service runnning A amelio : - Lancer automatiquement lorque le telephone se lance

--- app/src/main/java/com/example/activatebluewhencharginservicce/Main2Activity.java
+++ app/src/main/java/com/example/activatebluewhencharginservicce/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.example.activatebluewhencharginservicce;
 
 import android.app.IntentService;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,14 +13,13 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main2);
-
+        //Lier au service
         mainActivity main = new mainActivity();
 
         Intent intent = new Intent(this,mainActivity.class);
         startService(intent);
-        finish();//coucuo
-
-
-
+        finish();
     }
+
+
 }
--- app/src/main/java/com/example/activatebluewhencharginservicce/mainActivity.java
+++ app/src/main/java/com/example/activatebluewhencharginservicce/mainActivity.java
@@ -2,22 +2,45 @@ package com.example.activatebluewhencharginservicce;
 
 import android.app.IntentService;
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Looper;
 import android.util.Log;
-
+import android.widget.Toast;
 
 
 public class mainActivity extends Service {
-    private Looper looper;
-
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        super.onStartCommand(intent,flags,startId);
-        return START_STICKY;
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                    Intent filter = registerReceiver(null, new IntentFilter("android.hardware.usb.action.USB_STATE"));
+                    if (filter.getExtras().getBoolean("connected")) {
+                        bluetoothAdapter.enable();
+
+                    } else {
+                        bluetoothAdapter.disable();
+                    }
+                    try {
+                        Thread.sleep(2500);
+                    } catch (InterruptedException e){}
+                }
+
+            }
+        }).start();
+
+
+
+
+        return START_STICKY ;
     }
     @Override
     public IBinder onBind(Intent intent) {
@@ -27,5 +50,20 @@ public class mainActivity extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+
     }
+
+
+
+    private boolean USBisConnected(){
+        Intent intent = registerReceiver(null,new IntentFilter("android.hardware.usb.action.USB_STATE"));
+        return intent.getExtras().getBoolean("connected");
+    }
+
 }
+
+
+
+
+
+
